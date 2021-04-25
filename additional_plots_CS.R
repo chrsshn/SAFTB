49a5923a5ff93f54e7425e77fd08fac753b70db5
Chris Shin <chrsshn@gmail.com>
2021-04-24 22:57:22 -0500

    added spaghetti plot

--- additional_plots_CS.R
+++ additional_plots_CS.R
@@ -5,11 +5,17 @@ library (dplyr)
 load ("clean_base.df.rda")
 
 #scatter plot
-  ggplot(data=base.df) + 
-  geom_point (aes(x = .40 * Tot.Scr.Time, y = .6 * Pickups, color = Sequence))
+  ggplot(data=base.df, aes(x = .40 * Tot.Scr.Time, y = .6 * Pickups, color = Sequence)) + 
+  geom_point ()
   
   
 #scatter plot facetted by sex
-  ggplot(data=base.df) + 
-    geom_point (aes(x = .40 * Tot.Scr.Time, y = .6 * Pickups, color = Sequence)) +
+  ggplot(data=base.df, aes(x = .40 * Tot.Scr.Time, y = .6 * Pickups, color = Sequence)) + 
+    geom_point () +
     facet_grid(~sex)
+
+  
+#spaghetti plot 
+  ggplot(data=base.df, aes(x = index, y = Tot.Scr.Time, color = Sequence, group = pseudo_id)) + 
+  geom_line () 
+  
